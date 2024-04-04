b29376a8603260542c8ab9288c28e79fd47fbd9f
yuku-huang <kelly58516776@gmail.com>
2024-04-04 18:09:48 +0800

    Quartz sync: Apr 4, 2024, 6:09 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,8 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.Graph(),
-    Component.DesktopOnly(),
-    Component.TableOfContents(),
+    Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
 }
