83dc247e398762d39d09096ee9bf52ae226c8eb5
yuku_huang <kelly58516776@gmail.com>
2025-02-01 21:10:34 +0800

    vault backup: 2025-02-01 21:10:33

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🍵 羽空的部落格",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
